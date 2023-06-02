81862c3d007347304f618885f8acda5387434a44
VatsalRathod07 <vatsalrathod2005@gmail.com>
2023-06-02 14:19:55 +0530

    changed the style of search.jsx

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,65 +1,83 @@
 import React from "react";
 import { BiSearch, BiCurrentLocation } from "react-icons/bi";
-import { GiHotMeal } from "react-icons/gi"
+import { GiHotMeal } from "react-icons/gi";
 
 const Search = () => {
   return (
-    <div className="search grid gap-10 bg-LightGray rounded-md p-12 w-[80% mx-auto]">
-
+    <div className="search flex flex-col gap-5 rounded-md p-7 w-[80%] mx-auto border-b-2 border-LightGray">
       <form action="">
-
-        <div className="firstDiv flex justify-between items-center rounded-lg gap-3 bg-white shadow-md p-3 shadow-LightGray">
-
+        <div className="firstDiv flex flex-col gap-5 sm:flex-row sm:gap-3 border-LightGray rounded-md p-3 shadow-LightGray justify-center">
           <div className="flex gap-2 items-center">
             <BiSearch size={25} className="icon" />
-            <input type="text" placeholder="Search Restaurant" className="bg-transparent focus:outline-none w-full capitalize" />
+            <input
+              type="text"
+              placeholder="Search Restaurant"
+              className="bg-transparent focus:outline-none w-full capitalize"
+            />
           </div>
-
           <div className="flex gap-2 items-center">
             <GiHotMeal size={25} className="icon" />
-            <input type="text" placeholder="Search By cuisine" className="bg-transparent focus:outline-none w-full capitalize" />
+            <input
+              type="text"
+              placeholder="Search By cuisine"
+              className="bg-transparent focus:outline-none w-full capitalize"
+            />
           </div>
-
           <div className="flex gap-2 items-center">
             <BiCurrentLocation size={25} className="icon" />
-            <input type="text" placeholder="Search By Location" className="bg-transparent focus:outline-none w-full capitalize" />
+            <input
+              type="text"
+              placeholder="Search By Location"
+              className="bg-transparent focus:outline-none w-full capitalize"
+            />
           </div>
-
-          <button className="bg-blue-500 text-white p-3 h-full cursor-pointer px-10 rounded-md hover:bg-DeepBlue">Search</button>
-
+          <button className="bg-blue-500 text-white p-3 h-full cursor-pointer px-10 rounded-md hover:bg-DeepBlue">
+            Search
+          </button>
         </div>
-
       </form>
 
-      <div className="flex items-center gap-10 justify-center">
-
+      <div className="flex flex-col gap-5 sm:flex-row sm:gap-3 p-3 shadow-LightGray justify-center items-center">
         <div className="flex items-center gap-2">
-          <label htmlFor="relevance" className="text-[gray] font-semibold">Sort By:</label>
-          <select name="" id="relevance" className="bg-white rounded-sm py-1 pr-[1.75rem] focus:outline-none">
-            <option value="">Rating</option>
+          <label htmlFor="relevance" className="text-gray-500 font-semibold">
+            Sort By:
+          </label>
+          <select
+            name=""
+            id="relevance"
+            className="bg-white rounded-sm py-1 pr-[1.75rem] focus:outline-none border-2 border-LightGray w-28"
+          >
+            <option value="" className="border-2 border-LightGray">Rating</option>
           </select>
         </div>
-
         <div className="flex items-center gap-2">
-          <label htmlFor="level" className="text-[gray] font-semibold">cuisine:</label>
-          <select name="" id="level" className="bg-white rounded-sm py-1 pr-[1.75rem] focus:outline-none">
-            <option value="">Buffet</option>
+          <label htmlFor="level" className="text-gray-500 font-semibold">
+            Cuisine:
+          </label>
+          <select
+            name=""
+            id="level"
+            className="bg-white rounded-sm py-1 pr-[1.75rem] focus:outline-none border-2 border-LightGray w-28"
+          >
+            <option value="" className="border-2 border-LightGray">Buffet</option>
           </select>
         </div>
-
         <div className="flex items-center gap-2">
-          <label htmlFor="type" className="text-[gray] font-semibold">Hours:</label>
-          <select name="" id="type" className="bg-white rounded-sm py-1 pr-[1.75rem] focus:outline-none">
-            <option value="">Open now</option>
+          <label htmlFor="type" className="text-gray-500 font-semibold">
+            Hours:
+          </label>
+          <select
+            name=""
+            id="type"
+            className="bg-white rounded-sm py-1 pr-[1.75rem] focus:outline-none border-2 border-LightGray w-36"
+          >
+            <option value="" className="border-2 border-LightGray">Open now</option>
           </select>
         </div>
-
-        <span className="text-[gray] cursor-pointer">Clear All</span>
-
+        <span className="text-gray cursor-pointer">Clear All</span>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default Search; 
+export default Search;
